baf5f199dd25223e0b49eb1638c9f30f113a9c3d
Plcc18 <pedrolucascabral4@gmail.com>
2024-11-08 00:55:57 -0300

    function add item

---
+++ script.js
@@ -0,0 +1,39 @@
+const form = document.querySelector("form")
+const input = document.getElementById("input")
+const list = document.getElementById("list")
+
+form.onsubmit = (event) => {
+  event.preventDefault()
+  const newItem = input.value
+  addItem(newItem)
+}
+
+const addItem = (newItem) => {
+  const newLi = document.createElement("li")
+
+  const checkbox = document.createElement("input")
+  checkbox.type = "checkbox"
+  checkbox.classList.add("styled-checkbox")
+
+  const label = document.createElement("label")
+  label.classList.add("checkbox-label")
+
+  const span = document.createElement("span")
+
+  const p = document.createElement("p")
+  p.textContent = newItem
+
+  label.appendChild(span)
+  label.appendChild(p)
+  
+  const icon = document.createElement("ion-icon")
+  icon.name = "trash-outline"
+  
+  newLi.appendChild(checkbox)
+  newLi.appendChild(label)
+  newLi.appendChild(icon)
+
+  list.appendChild(newLi)
+
+  input.value = ""
+}
